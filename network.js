973c8a7372febc999097c444c62061f4a3d504e9
Teresa <teresa@eterea.uk>
2020-12-28 12:52:52 -0500

    pretty anime

--- network.js
+++ network.js
@@ -114,7 +114,15 @@ window.client.on("CLIENTSTATUS", (user,status) => {
 
 window.client.on("BATTLEOPENED",(battleid, type, natType, founder, ip, port, maxPlayer, passworded, rank, mapHesh, engineName, engineVersion, map, title, gameName, channel) => {
 	console.log("BATTLE OPENED!!!!!!!!!!!!!!")
-	document.getElementById("gameEntry").innerHTML+="<!--to be inserted by js--><li class = \"gameSubEntry\" style=\"display:inline;display:inline-block;position: relative;\" id=battleEntry"+battleid+"> <p id=\""+battleid+"\"onclick=\"if (window.isExited==true){window.client.joinBattle(&#39;"+battleid+"&#39;);}else{exitGem(window.nowinBattle);}\"class=\"gameInnerSubEntryTXT\" style=\"overflow: hidden; font-family: JuneBug2; position: relative; cursor:pointer;background : #2196f3;  margin: 20px ; padding: 25px; mix-blend-mode: screen; font-weight: bold;\" type=\"button\" >"+founder +"</br>"+map+"<p id=\"title"+battleid+"\" style=\"font-size: 1.5vw; color: #255784; position:absolute; top: 65px ;background-color: rgba(255,255,255,0.85); padding:2px; box-shadow: 0 0 10px #2196f3, 0 0 40px #2196f3, 0 0 80px #2196f3; mix-blend-mode: screen;\">"+title+"</p></p></li><!--to be inserted by js-->"
+	
+	
+	var subEntry = document.createElement('li');
+	subEntry.classList.add('gameSubEntry');
+	subEntry.id="battleEntry"+battleid
+	subEntry.innerHTML = "<p id=\""+battleid+"\"onclick=\"if (window.isExited==true){window.client.joinBattle(&#39;"+battleid+"&#39;);}else{exitGem(window.nowinBattle);}\"class=\"gameInnerSubEntryTXT\" style=\"overflow: hidden; font-family: JuneBug2; position: relative; cursor:pointer;background : #2196f3;  margin: 20px ; padding: 25px; mix-blend-mode: screen; font-weight: bold;\" type=\"button\" >"+founder +"</br>"+map+"<p id=\"title"+battleid+"\" style=\"font-size: 1.5vw; color: #255784; position:absolute; top: 65px ;background-color: rgba(255,255,255,0.85); padding:2px; box-shadow: 0 0 10px #2196f3, 0 0 40px #2196f3, 0 0 80px #2196f3; mix-blend-mode: screen;\">"+title+"</p></p>";
+	document.getElementById("gameEntry").appendChild(subEntry);
+	
+	
 	
 });
 
