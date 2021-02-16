eb161e8a570bebc46b322e4c366effe71fd39aeb
Teresa <teresa@eterea.uk>
2021-02-15 21:49:26 -0500

    better infopanel menu

--- freunds.js
+++ freunds.js
@@ -1,18 +1,45 @@
 function frdPut(page="main",name,battle,isBtlFrd=false){
 	
 	if (isBtlFrd){
-		document.getElementById("friendlimitingFrame"+page).innerHTML +="<div style=\"margin:1%;\" class=\"userCard\" id=\"userCard"+page+name+"\" ><div style=\"overflow:hidden; position: absolute; height: 100%; top: 0; left:0%; width:100%; background-color: rgba(105,105,105,0.3);display:inline-block;font-size:7vh;text-transform: uppercase;filter: drop-shadow(0.4rem 0.5rem 0.2rem rgba(200,200,200,0.6));color:white;\" id=\"cardLabel"+page+name+"\">A</div><div class=\"freundBody\" onclick=\"chTeams(\'"+name+"\')\" style=\"opacity:0.9;top:0;height:100%;position:absolute;left:15%;background:rgba(33,150,243,1);width:85%;\"><span style=\"font-weight:900;font-size:2rem;\">"+name+"</span><img src=\"assets/thea.png\" style=\"position:absolute;width:80%;opacity:0.3;top:37%;left:5%;z-index:-1;\"></div><div class=\"isLeader\" id=\"cardIsLeader"+page+name+"\"onclick=\"chLeader('"+name+"')\" style=\"overflow:visible;position:absolute; bottom:-9%;right:-5%;background:white;padding:2%;\">Leader</div></div>";
+		document.getElementById("friendlimitingFrame"+page).innerHTML +="<div style=\"margin:1%;\" class=\"userCard\" onmouseenter=\"showFrdOptions('frdOptions"+page+name+"')\" onmouseleave=\"hideFrdOptions('frdOptions"+page+name+"')\" id=\"userCard"+page+name+"\" ><div style=\"overflow:hidden; position: absolute; height: 100%; top: 0; left:0%; width:100%; background-color: rgba(105,105,105,0.3);display:inline-block;font-size:7vh;text-transform: uppercase;filter: drop-shadow(0.4rem 0.5rem 0.2rem rgba(200,200,200,0.6));color:white;\" onclick=\"chTeams(\'"+name+"\')\" id=\"cardLabel"+page+name+"\">A</div><div class=\"freundBody\"  style=\"opacity:0.9;top:0;height:100%;position:absolute;right:0;background:rgba(33,150,243,1);width:85%;\"><span style=\"font-weight:900;font-size:2rem;\">"+name+"</span><img src=\"assets/thea.png\" style=\"position:absolute;width:80%;opacity:0.3;top:37%;left:5%;z-index:-1;\"></div><div class=\"isLeader\" id=\"cardIsLeader"+page+name+"\"  style=\"overflow:visible;position:absolute; bottom:-9%;right:-5%;background:white;padding:2%;\">Leader</div><div class=\"frdOptions\" id=\"frdOptions"+page+name+"\" style=\"background:rgba(33,150,243,0.8);visibility:hidden; top:0;position:absolute;right:0%;width:70%;height:100%;\"><div style=\"height:90%;top:5%;width:2px;background-color:white;top:5%;position:absolute;\" class=\"verticalLine\"></div><div  id='frdSubOptions1'  class=\"frdSubOptions frdSubOptionsAnim1\" onclick=\"chLeader('"+name+"')\" style=\"width:95%; position:absolute;left:-55%;\">Leader Status</div><div id='frdSubOptions2' class=\"frdSubOptions frdSubOptionsAnim2\" style=\"width:95%; position:absolute;left:-55%;\">Joint Tactics</div><div id='frdSubOptions3' class=\"frdSubOptions frdSubOptionsAnim3\" style=\"width:95%; position:absolute;left:-55%;\">Dismiss Personel</div><div id='frdSubOptions4' class=\"frdSubOptionsAnim4 frdSubOptions\" style=\"width:95%; position:absolute;left:-55%;\">Save Colleague</div></div></div>";
 	}
 	else{
 		document.getElementById("friendlimitingFrame"+page).innerHTML +="<div class=\"userCard\" style=\"margin:1%;\" id=\"userCard"+page+name+"\" ><div style=\"overflow:hidden; position: absolute; height: 100%; top: 0; left:0%; width:100%; background-color: rgba(105,105,105,0.3);display:inline-block;font-size:7vh;\"></div><div class=\"freundBody\" style=\"opacity:0.9;width:85%;top:0;height:100%; background:rgba(33,150,243,1);position:absolute;left:15%;\"><span style=\"font-family: JuneBug2\">"+name+"</span><br><span>In "+battle+"<br> For: -\\- hr  -\\-min</span></div></div>";
 	}
 }
 
+function showFrdOptions(optionID)
+{
+
+	
+	// animation rerun
+	var elm = document.getElementById(optionID)
+	var newone = elm.cloneNode(true);
+	elm.parentNode.replaceChild(newone, elm);
+
+	//
+	
+	
+	
+	console.log('mouse over')
+	window.revealFrdSubOptions=setTimeout(function(){ document.getElementById(optionID).style.visibility='visible' }, 350);
+	
+	//document.getElementById(optionID).innerHTML+='<! -- -->'
+}
+
+function hideFrdOptions(optionID){
+	console.log('mouse leave')
+	document.getElementById(optionID).style.visibility='hidden'
+	clearTimeout(window.revealFrdSubOptions)
+}
+
+
+
 function aiPut(page,name){
 	
 	
 	window.ppl[name]='a';
-	document.getElementById("friendlimitingFrame"+page).innerHTML +="<div style=\"margin:1%;\" class=\"userCard\" id=\"userCard"+page+name+"\" ><div style=\"overflow:hidden; position: absolute; height: 100%; top: 0; left:0%; width:100%; background-color: rgba(105,105,105,0.3);display:inline-block;font-size:7vh;text-transform: uppercase;filter: drop-shadow(0.4rem 0.5rem 0.2rem rgba(200,200,200,0.6));color:white;\" id=\"cardLabel"+page+name+"\">A</div><div class=\"freundBody\" onclick=\"chTeams(\'"+name+"\')\" style=\"opacity:0.9;top:0;height:100%;position:absolute;left:15%;background:rgba(33,150,243,1);width:85%;\"><span style=\"font-weight:900;font-size:2rem;\">"+name+"</span><img src=\"assets/theaAI.png\" style=\"position:absolute;width:60%;opacity:0.3;top:37%;left:5%;z-index:-1;\"></div><div class=\"isLeader\" id=\"cardIsLeader"+page+name+"\"onclick=\"chLeader('"+name+"')\" style=\"overflow:visible;position:absolute; bottom:-9%;right:-5%;background:white;padding:2%;\">Leader</div><div class=\"aiKill\" onclick=\"chatAIKill('"+page+"','"+name+"')\"style=\"overflow:visible;position:absolute;top:-9%;right:-5%;color:white;padding-left: 2%;padding-right: 2%;\">X</div></div>";
+	document.getElementById("friendlimitingFrame"+page).innerHTML +="<div style=\"margin:1%;\" class=\"userCard\" onmouseenter=\"showFrdOptions('frdOptions"+page+name+"')\" onmouseleave=\"hideFrdOptions('frdOptions"+page+name+"')\" id=\"userCard"+page+name+"\" ><div style=\"overflow:hidden; position: absolute; height: 100%; top: 0; left:0%; width:100%; background-color: rgba(105,105,105,0.3);display:inline-block;font-size:7vh;text-transform: uppercase;filter: drop-shadow(0.4rem 0.5rem 0.2rem rgba(200,200,200,0.6));color:white;\" id=\"cardLabel"+page+name+"\">A</div><div class=\"freundBody\" onclick=\"chTeams(\'"+name+"\')\" style=\"opacity:0.9;top:0;height:100%;position:absolute;left:15%;background:rgba(33,150,243,1);width:85%;\"><span style=\"font-weight:900;font-size:2rem;\">"+name+"</span><img src=\"assets/theaAI.png\" style=\"position:absolute;width:60%;opacity:0.3;top:37%;left:5%;z-index:-1;\"></div><div class=\"frdOptions\" id=\"frdOptions"+page+name+"\" style=\"background:rgba(33,150,243,0.8);visibility:hidden; top:0;position:absolute;right:0%;width:70%;height:100%;\"><div style=\"height:90%;top:5%;width:2px;background-color:white;top:5%;position:absolute;\" class=\"verticalLine\"></div><div  id='frdSubOptions1'  class=\"frdSubOptions frdSubOptionsAnim1\"  style=\"width:95%; position:absolute;left:-55%;\">-/-</div><div id='frdSubOptions2' class=\"frdSubOptions frdSubOptionsAnim2\" style=\"width:95%; position:absolute;left:-55%;\">Attach</div><div id='frdSubOptions3' class=\"frdSubOptions frdSubOptionsAnim3\" onclick=\"chatAIKill('"+page+"','"+name+"')\" style=\"width:95%; position:absolute;left:-55%;\">Uninit</div><div id='frdSubOptions4' class=\"frdSubOptionsAnim4 frdSubOptions\" style=\"width:95%; position:absolute;left:-55%;\">-/-</div></div></div>";
 
 }
 
@@ -46,7 +73,7 @@ function chLeader(usr){
 		chatAssignLeader(window.ppl[usr]) //tell the function to look up the leader for this team
 	}
 	else{
-		document.getElementById("cardIsLeader"+window.nowinBattle+usr).style.opacity="0.5"
+		document.getElementById("cardIsLeader"+window.nowinBattle+usr).style.opacity="0"
 		window.teamLeaders[window.ppl[usr]]='None'
 	}
 }
