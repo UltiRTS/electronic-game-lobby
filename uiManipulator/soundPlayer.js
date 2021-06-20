8992acbbfa421c777842dc92d13cf75fb5092b03
shushuxin <shushuxin@ebupt.com>
2021-06-20 16:23:41 +0800

    Using path.join to make assets path and formart code.

--- uiManipulator/soundPlayer.js
+++ uiManipulator/soundPlayer.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 window.isPlaying=false
 
 // convert nodejs buffer to ArrayBuffer 
@@ -13,123 +15,110 @@ function toArrayBuffer(buf) {
 
 // new one
 function playSound(file, loop) {
-	
-	
-	
 	if(!window.audioCtx) {
 		window.audioCtx = new AudioContext();
-		
 		window.contextGain = window.audioCtx.createGain();
-		
 		// connect to context
 		window.contextGain.connect(window.audioCtx.destination);
-		actuallyPlay()
-		
+		actuallyPlay();
 	} else {
-		
-		
 		//window.contextGain.gain.linearRampToValueAtTime(0, context.currentTime+fadeDuration);
 		var instaVolDuringFade=50
 		loading(true)
-		window.ramp=setInterval(
-			function(){
-				if(instaVolDuringFade<=0){clearInterval(window.ramp);actuallyPlay()}
-				else{instaVolDuringFade-=4;window.contextGain.gain.setValueAtTime((window.userVolume/100)*(instaVolDuringFade/50), window.audioCtx.currentTime)}
-				
-			},100)
-		
+		window.ramp=setInterval(() => {
+			if (instaVolDuringFade <= 0) {
+				clearInterval(window.ramp);
+				actuallyPlay();
+			} else {
+				instaVolDuringFade-=4;
+				window.contextGain.gain.setValueAtTime(
+					(window.userVolume/100) * (instaVolDuringFade/50),
+					window.audioCtx.currentTime
+				);
+			}
+		}, 100)
 	}
-	
 
-	
 	function actuallyPlay(){
-	//window.contextGain.gain.setValueAtTime(window.userVolume/100, window.audioCtx.currentTime);
+		//window.contextGain.gain.setValueAtTime(window.userVolume/100, window.audioCtx.currentTime);
 		window.contextGain.gain.setValueAtTime(window.userVolume/100, window.audioCtx.currentTime);
-		var prefix = __dirname+'/assets/';
+		var assets_dir = path.join(__dirname, 'assets');
 		
 		try {
-				window.sourceIntro.disconnect(window.contextGain);
-				window.sourceLoop.disconnect(window.audioDelay);
-				
-			}
-			catch{}
-			window.isPlaying = true;
+			window.sourceIntro.disconnect(window.contextGain);
+			window.sourceLoop.disconnect(window.audioDelay);
+		} catch {
+		}
+
+		window.isPlaying = true;
 		if(loop) {
 			// if is playing disconnect it
-			
-
-			
-
 			window.sourceIntro = window.audioCtx.createBufferSource();
 			window.sourceLoop = window.audioCtx.createBufferSource();
 
-
-			var bufferIntro = toArrayBuffer(fs.readFileSync(prefix + file));
-			var bufferLoop = toArrayBuffer(fs.readFileSync(prefix + 'loop_' + file));
-
+			var bufferIntro = toArrayBuffer(fs.readFileSync(path.join(assets_dir, file)));
+			var bufferLoop = toArrayBuffer(fs.readFileSync(path.join(assets_dir, 'loop_' + file)));
 
 			window.audioCtx.decodeAudioData(bufferIntro, buf => {
 				var duration = buf.duration;
 				//sourceIntro.buffer = buf;
-				window.sourceIntro.buffer = buf
-
+				window.sourceIntro.buffer = buf;
 				//window.sourceIntro.connect(window.sourceIntro);
 				// intro fade in
 				window.sourceIntro.connect(window.contextGain);
 				// set to disconnect
 				//window.sourceIntro = gainIntro;
-
 				//gainIntro.gain.setValueAtTime(0, context.currentTime+10);
-				
-				
-				
 				window.audioDelay = window.audioCtx.createDelay(duration-0.03);
 				//var delayNode = context.createDelay()
 				window.audioDelay.delayTime.value = duration-0.03;
 				window.audioDelay.connect(window.contextGain);
 
 				window.audioCtx.decodeAudioData(bufferLoop, buf => {
-					
 					window.sourceLoop.buffer = buf;
 					sourceLoop.loop = true;
-
 					// set loop fade in
 					//gainLoop.gain.setValueAtTime(0, context.currentTime);
 					//gainLoop.gain.linearRampToValueAtTime(1, context.currentTime + 1);
-
 					window.sourceLoop.connect(window.audioDelay);
-					try{loading(false)}
-					catch{}
-					
+					try {
+						loading(false);
+					} catch {
+					}
 					window.sourceLoop.start(0);window.sourceIntro.start(0);
-					
 				});
 			});
-
 		} else {
 			window.sourceIntro = window.audioCtx.createBufferSource();
-			var buffer = toArrayBuffer(fs.readFileSync(prefix + file));
+			var buffer = toArrayBuffer(fs.readFileSync(path.join(assets_dir, file)));
 			window.audioCtx.decodeAudioData(buffer, buf => {
 				window.sourceIntro.buffer = buf;
 				window.sourceIntro.connect(window.contextGain);
 				//window.contextGain.gain.setValueAtTime(window.userVolume/100, window.audioCtx.currentTime);source.start(0);},fadeDuration*1000
-				try{loading(false)}
-		catch{}
-				window.sourceIntro.start(0)
+				try {
+					loading(false);
+				} catch {
+				}
+				window.sourceIntro.start(0);
 			});
 		}
-	
 	}
 }
-	function stopSound(){
-		var instaVolDuringFade=50
-		window.ramp=setInterval(
-			function(){
-				if(instaVolDuringFade<=0){clearInterval(window.ramp);}
-				else{instaVolDuringFade-=4;window.contextGain.gain.setValueAtTime((window.userVolume/100)*(instaVolDuringFade/50), window.audioCtx.currentTime)}
-				
-			},100)
-		
-	}
 
-playSound('title.wav',true)
+function stopSound(){
+	var instaVolDuringFade = 50;
+	window.ramp=setInterval(
+		() => {
+			if(instaVolDuringFade <= 0){
+				clearInterval(window.ramp);
+			} else {
+				instaVolDuringFade -= 4;
+				window.contextGain.gain.setValueAtTime(
+					(window.userVolume/100) * (instaVolDuringFade/50),
+					window.audioCtx.currentTime
+				);
+			}
+	}, 100);
+}
+
+playSound('title.wav',true);
